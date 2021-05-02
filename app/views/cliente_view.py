a70b0841e3b227227bdc48c0a40347c4fa17a0db
Vitor <vitorsoaresk@gmail.com>
2021-05-02 15:29:08 -0300

    adicionando templates com css/js e atualizando as views

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 
-@app.route('/')
-def hello():
-    return 'hello world!'
+@app.route('/ola', defaults={'nome': None}, methods={'GET', 'POST'})
+@app.route('/ola/<string:nome>')
+def hello(nome):
+    return render_template('clientes/teste.html', usuario=nome)
